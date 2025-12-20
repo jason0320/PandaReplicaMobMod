f0de8b0388fd1a906876e86cbc1ef67bf99ff0e7
jason0320 <jasonnosaj0320@gmail.com>
2025-12-20 15:50:09 +0800

    加入專案檔案。

---
+++ Chara_RestockEquip_Patch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace Mod_PandaReplicaMobMod
+{
+    [HarmonyPatch(typeof(Chara), "RestockEquip")]
+    internal class Chara_RestockEquip_Patch
+    {
+        public static void Postfix(Chara __instance, bool onCreate)
+        {
+
+            if (__instance.id == "hp_earth_omega")
+            { 
+                if (onCreate)
+                {
+                    Thing thing = ThingGen.Create("blunt_earth");
+                    thing.SetReplica(on: true);
+                    thing.rarity = Rarity.Normal;
+                    __instance.AddThing(thing);
+                    __instance.body.Equip(thing);
+                    thing.rarity = Rarity.Artifact;
+                }
+            }
+
+            if (__instance.id == "hp_elemental_beta")
+            {
+                if (onCreate)
+                {
+                    Thing thing = ThingGen.Create("staff_element");
+                    thing.SetReplica(on: true);
+                    thing.rarity = Rarity.Normal;
+                    __instance.AddThing(thing);
+                    __instance.body.Equip(thing);
+                    thing.rarity = Rarity.Artifact;
+                }
+            }
+        }
+    }
+}
---
+++ Mod_PandaReplicaMobMod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+using BepInEx;
+using HarmonyLib;
+
+namespace Mod_PandaReplicaMobMod
+{
+    [BepInPlugin("panda.replicamob.mod", "Panda's Replica Mob Mod", "1.0.0.0")]
+    internal class Mod_PandaReplicaMobMod : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            var harmony = new Harmony("Panda's Replica Mob Mod");
+            harmony.PatchAll();
+        }
+        private void OnStartCore()
+        {
+            string pathToExcelFile = Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location) + "/ReplicaMob.xlsx";
+            SourceManager sources = Core.Instance.sources;
+            ModUtil.ImportExcel(pathToExcelFile, "Chara", sources.charas);
+        }
+    }
+}
