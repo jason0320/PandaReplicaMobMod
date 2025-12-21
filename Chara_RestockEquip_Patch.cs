8ffc50ab0acef710456105480463b1ec44a31abc
jason0320 <jasonnosaj0320@gmail.com>
2025-12-21 11:38:01 +0800

    Add more mobs

--- Chara_RestockEquip_Patch.cs
+++ Chara_RestockEquip_Patch.cs
@@ -36,6 +36,45 @@ namespace Mod_PandaReplicaMobMod
                     thing.rarity = Rarity.Artifact;
                 }
             }
+
+            if (__instance.id == "hp_mech_warmonger")
+            {
+                if (onCreate)
+                {
+                    Thing thing = ThingGen.Create("warmonger");
+                    thing.SetReplica(on: true);
+                    thing.rarity = Rarity.Normal;
+                    __instance.AddThing(thing);
+                    __instance.body.Equip(thing);
+                    thing.rarity = Rarity.Artifact;
+                }
+            }
+
+            if (__instance.id == "hp_robot")
+            {
+                if (onCreate)
+                {
+                    Thing thing = ThingGen.Create("sword_muramasa2");
+                    thing.SetReplica(on: true);
+                    thing.rarity = Rarity.Normal;
+                    __instance.AddThing(thing);
+                    __instance.body.Equip(thing);
+                    thing.rarity = Rarity.Artifact;
+                }
+            }
+
+            if (__instance.id == "hp_farmerman")
+            {
+                if (onCreate)
+                {
+                    Thing thing = ThingGen.Create("scythe_kumi");
+                    thing.SetReplica(on: true);
+                    thing.rarity = Rarity.Normal;
+                    __instance.AddThing(thing);
+                    __instance.body.Equip(thing);
+                    thing.rarity = Rarity.Artifact;
+                }
+            }
         }
     }
 }
